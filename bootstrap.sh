9c0704f6d2b1cd8f5b0ef4fbd013166ab745150d
Daniel Gibbs <daniel@danielgibbs.name>
2015-02-26 22:01:32 +1300

    Handle spaces in paths properly. Fixes #1

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,18 @@
 #
 
 FILE_LOCATION="https://raw.githubusercontent.com/DanielGibbsNZ/bootstrap/master"
-INSTALL_LOCATION=${HOME}
+INSTALL_LOCATION="${HOME}"
 NANORC_LOCATIONS=("/usr/share/nano" "/usr/local/share/nano")
 RC_FILES=(".bashrc" ".vimrc" ".nanorc" ".gitconfig")
 
 # File installation function.
 function install_file {
-	FILE=$1
-	DEST=$2
+	FILE="$1"
+	DEST="$2"
 	printf "Installing ${FILE}... "
-	if [ -e ${DEST}/${FILE} ]; then
+	if [ -e "${DEST}/${FILE}" ]; then
 		# If file already exists check for differences.
-		if cmp -s ${DEST}/${FILE} ${TMP_DIR}/${FILE}; then
+		if cmp -s "${DEST}/${FILE}" "${TMP_DIR}/${FILE}"; then
 			echo -e "\033[32mDONE\033[0m"
 		else
 			# If there are differences, ask the user which version they want.
@@ -24,19 +24,19 @@ function install_file {
 			while [ "${REPLACE}" != "y" -a "${REPLACE}" != "n" -a "${REPLACE}" != "m" ]; do
 				read -p "Replace original file (d for diff, m for manual merge)? (y/n/d/m) " REPLACE
 				if [ "${REPLACE}" = "d" ]; then
-					${DIFF} ${DEST}/${FILE} ${TMP_DIR}/${FILE} | less -r
+					${DIFF} "${DEST}/${FILE}" "${TMP_DIR}/${FILE}" | less -r
 				fi
 			done
 			if [ "${REPLACE}" = "y" ]; then
-				cp ${TMP_DIR}/${FILE} ${DEST}/${FILE}
+				cp "${TMP_DIR}/${FILE}" "${DEST}/${FILE}"
 			elif [ "${REPLACE}" = "m" ]; then
-				MERGE_FILE=${DEST}/${FILE}.bootstrap
-				cp ${TMP_DIR}/${FILE} ${DEST}/${FILE}.bootstrap
+				MERGE_FILE="${DEST}/${FILE}.bootstrap"
+				cp "${TMP_DIR}/${FILE}" "${DEST}/${FILE}.bootstrap"
 				echo "The downloaded version of ${FILE} can be found in ${MERGE_FILE} for manual merging."
 			fi
 		fi
 	else
-		cp ${TMP_DIR}/${FILE} ${DEST}/${FILE}
+		cp "${TMP_DIR}/${FILE}" "${DEST}/${FILE}"
 		echo -e "\033[32mDONE\033[0m"
 	fi
 }
@@ -44,7 +44,7 @@ function install_file {
 # Process arguments.
 if [ "$1" = "-d" -o "$1" = "--dest" ]; then
 	if [ "$2" -a -d "$2" ]; then
-		INSTALL_LOCATION=$2
+		INSTALL_LOCATION="$2"
 	else
 		echo "Invalid destination: $2"
 		exit 1
@@ -82,7 +82,7 @@ else
 fi
 
 # Create temporary directory.
-TMP_DIR=$(mktemp -dq /tmp/bootstrap.XXXXXX)
+TMP_DIR="$(mktemp -dq /tmp/bootstrap.XXXXXX)"
 if [ $? -ne 0 ]; then
 	echo "Unable to create temporary directory."
 	exit 1
@@ -91,9 +91,9 @@ trap "rm -rf ${TMP_DIR}; exit 0" EXIT SIGINT SIGKILL SIGTERM
 
 # Download and install RC files.
 echo -e "\033[37m===>\033[0m CONFIG FILES \033[37m<===\033[0m"
-for FILE in ${RC_FILES[*]}; do
+for FILE in "${RC_FILES[@]}"; do
 	printf "Downloading ${FILE}... "
-	if ${DOWNLOAD} ${FILE_LOCATION}/${FILE} ${OUTPUT} ${TMP_DIR}/${FILE}; then
+	if ${DOWNLOAD} "${FILE_LOCATION}/${FILE}" ${OUTPUT} "${TMP_DIR}/${FILE}"; then
 		echo -e "\033[32mDONE\033[0m"
 	else
 		echo -e "\033[31mFAILED\033[0m"
@@ -102,26 +102,26 @@ for FILE in ${RC_FILES[*]}; do
 
 	# Search for nanorc files and add them to .nanorc.
 	if [ "${FILE}" = ".nanorc" ]; then
-		for NANORC_LOCATION in ${NANORC_LOCATIONS[*]}; do
-			if [ -d ${NANORC_LOCATION} ]; then
-				echo "" >> ${TMP_DIR}/${FILE}
-				for NANORC_FILE in $(ls ${NANORC_LOCATION}/*.nanorc 2>/dev/null); do
-					echo "include ${NANORC_FILE}" >> ${TMP_DIR}/${FILE}
+		for NANORC_LOCATION in "${NANORC_LOCATIONS[@]}"; do
+			if [ -d "${NANORC_LOCATION}" ]; then
+				echo "" >> "${TMP_DIR}/${FILE}"
+				for NANORC_FILE in $(ls "${NANORC_LOCATION}"/*.nanorc 2>/dev/null); do
+					echo "include ${NANORC_FILE}" >> "${TMP_DIR}/${FILE}"
 				done
 			fi
 		done
 	fi
 
-	install_file ${FILE} ${INSTALL_LOCATION}
+	install_file "${FILE}" "${INSTALL_LOCATION}"
 done
 
 # OS X specific setup.
 if [ "${PLATFORM}" = "OS X" ]; then
 	# Download and install .profile.
 	printf "Downloading .profile... "
-	if ${DOWNLOAD} ${FILE_LOCATION}/osx/.profile ${OUTPUT} ${TMP_DIR}/.profile; then
+	if ${DOWNLOAD} "${FILE_LOCATION}/osx/.profile" ${OUTPUT} "${TMP_DIR}/.profile"; then
 		echo -e "\033[32mDONE\033[0m"
-		install_file .profile ${INSTALL_LOCATION}
+		install_file ".profile" "${INSTALL_LOCATION}"
 	else
 		echo -e "\033[31mFAILED\033[0m"
 	fi
@@ -130,15 +130,15 @@ if [ "${PLATFORM}" = "OS X" ]; then
 	echo
 	echo -e "\033[37m===>\033[0m SCRIPTS \033[37m<===\033[0m"
 	OSX_SCRIPTS=("delxattr")
-	if [ ! -d ${INSTALL_LOCATION}/Scripts ]; then
-		mkdir -p ${INSTALL_LOCATION}/Scripts
+	if [ ! -d "${INSTALL_LOCATION}/Scripts" ]; then
+		mkdir -p "${INSTALL_LOCATION}/Scripts"
 	fi
-	for SCRIPT in ${OSX_SCRIPTS[*]}; do
+	for SCRIPT in "${OSX_SCRIPTS[@]}"; do
 		printf "Downloading ${SCRIPT}... "
-		if ${DOWNLOAD} ${FILE_LOCATION}/osx/${SCRIPT} ${OUTPUT} ${TMP_DIR}/${SCRIPT}; then
+		if ${DOWNLOAD} "${FILE_LOCATION}/osx/${SCRIPT}" ${OUTPUT} "${TMP_DIR}/${SCRIPT}"; then
 			echo -e "\033[32mDONE\033[0m"
-			chmod +x ${TMP_DIR}/${SCRIPT}
-			install_file ${SCRIPT} ${INSTALL_LOCATION}/Scripts
+			chmod +x "${TMP_DIR}/${SCRIPT}"
+			install_file "${SCRIPT}" "${INSTALL_LOCATION}/Scripts"
 		else
 			echo -e "\033[31mFAILED\033[0m"
 		fi
@@ -149,24 +149,24 @@ if [ "${PLATFORM}" = "OS X" ]; then
 	echo -e "\033[37m===>\033[0m HOMEBREW \033[37m<===\033[0m"
 	if command -v brew &>/dev/null; then
 		printf "Downloading Homebrew formula list... "
-		if ${DOWNLOAD} ${FILE_LOCATION}/osx/homebrew-formulae ${OUTPUT} ${TMP_DIR}/homebrew-formulae; then
+		if ${DOWNLOAD} "${FILE_LOCATION}/osx/homebrew-formulae" ${OUTPUT} "${TMP_DIR}/homebrew-formulae"; then
 			echo -e "\033[32mDONE\033[0m"
 
 			# Check for missing formulae.
-			brew list > ${TMP_DIR}/homebrew-installed
+			brew list > "${TMP_DIR}/homebrew-installed"
 			if command -v brew-cask &>/dev/null; then
-				brew cask list >> ${TMP_DIR}/homebrew-installed
+				brew cask list >> "${TMP_DIR}/homebrew-installed"
 			fi
 			FORMULAE_TO_INSTALL=()
-			for FORMULA in $(cat ${TMP_DIR}/homebrew-formulae); do
+			for FORMULA in $(cat "${TMP_DIR}/homebrew-formulae"); do
 				LAST_PART=$(echo "${FORMULA}" | grep -o "[^/]*$")
-				if ! grep "^${LAST_PART}$" -q ${TMP_DIR}/homebrew-installed; then
+				if ! grep "^${LAST_PART}$" -q "${TMP_DIR}/homebrew-installed"; then
 					echo -e "${LAST_PART} is not installed."
 					FORMULAE_TO_INSTALL+=("${FORMULA}")
 				fi
 			done
 			if [ ${#FORMULAE_TO_INSTALL[@]} -gt 0 ]; then
-				echo -e "You can install the missing formulae with \033[36;1mbrew install ${FORMULAE_TO_INSTALL[*]}\033[0m."
+				echo -e "You can install the missing formulae with \033[36;1mbrew install ${FORMULAE_TO_INSTALL[@]}\033[0m."
 			else
 				echo "All formulae installed."
 			fi
@@ -187,20 +187,20 @@ if [ "${PLATFORM}" = "Linux" ]; then
 	echo
 	echo -e "\033[37m===>\033[0m PACKAGES \033[37m<===\033[0m"
 	if command -v apt-get &>/dev/null; then
-		if [ ! -w /var/lib/dpkg ]; then
+		if [ ! -w "/var/lib/dpkg" ]; then
 			APT_GET="sudo apt-get"
 		else
 			APT_GET="apt-get"
 		fi
 		printf "Downloading Aptitude package list... "
-		if ${DOWNLOAD} ${FILE_LOCATION}/linux/aptitude-packages ${OUTPUT} ${TMP_DIR}/aptitude-packages; then
+		if ${DOWNLOAD} "${FILE_LOCATION}/linux/aptitude-packages" ${OUTPUT} "${TMP_DIR}/aptitude-packages"; then
 			echo -e "\033[32mDONE\033[0m"
 
 			# Check for missing packages.
-			dpkg --get-selections | awk '{print $1}' > ${TMP_DIR}/aptitude-installed
+			dpkg --get-selections | awk '{print $1}' > "${TMP_DIR}/aptitude-installed"
 			PACKAGES_TO_INSTALL=()
-			for PACKAGE in $(cat ${TMP_DIR}/aptitude-packages); do
-				if ! grep "^${PACKAGE}$" -q ${TMP_DIR}/aptitude-installed; then
+			for PACKAGE in $(cat "${TMP_DIR}/aptitude-packages"); do
+				if ! grep "^${PACKAGE}$" -q "${TMP_DIR}/aptitude-installed"; then
 					echo -e "${PACKAGE} is not installed."
 					PACKAGES_TO_INSTALL+=("${PACKAGE}")
 				fi
@@ -224,21 +224,21 @@ echo
 echo -e "\033[37m===>\033[0m PIP \033[37m<===\033[0m"
 if command -v python &>/dev/null && command -v pip &>/dev/null; then
 	# This can be used when automatically installing packages.
-	SITE_PACKAGE_DIR=$(python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()" &>/dev/null)
-	if [ ! -w ${SITE_PACKAGE_DIR} ]; then
+	SITE_PACKAGE_DIR="$(python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()" 2>/dev/null)"
+	if [ ! -w "${SITE_PACKAGE_DIR}" ]; then
 		PIP="sudo pip"
 	else
 		PIP="pip"
 	fi
 	printf "Downloading pip formula list... "
-	if ${DOWNLOAD} ${FILE_LOCATION}/pip-packages ${OUTPUT} ${TMP_DIR}/pip-packages; then
+	if ${DOWNLOAD} "${FILE_LOCATION}/pip-packages" ${OUTPUT} "${TMP_DIR}/pip-packages"; then
 		echo -e "\033[32mDONE\033[0m"
 
 		# Check for missing packages.
-		pip freeze > ${TMP_DIR}/pip-installed
+		pip freeze > "${TMP_DIR}/pip-installed"
 		PACKAGES_TO_INSTALL=()
-		for PACKAGE in $(cat ${TMP_DIR}/pip-packages); do
-			if ! grep "^${PACKAGE}==" -q ${TMP_DIR}/pip-installed; then
+		for PACKAGE in $(cat "${TMP_DIR}/pip-packages"); do
+			if ! grep "^${PACKAGE}==" -q "${TMP_DIR}/pip-installed"; then
 				echo -e "${PACKAGE} is not installed."
 				PACKAGES_TO_INSTALL+=("${PACKAGE}")
 			fi
@@ -268,21 +268,21 @@ echo
 echo -e "\033[37m===>\033[0m PIP3 \033[37m<===\033[0m"
 if command -v python3 &>/dev/null && command -v pip3 &>/dev/null; then
 	# This can be used when automatically installing packages.
-	SITE_PACKAGE_DIR=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())" &>/dev/null)
-	if [ ! -w ${SITE_PACKAGE_DIR} ]; then
+	SITE_PACKAGE_DIR="$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())" 2>/dev/null)"
+	if [ ! -w "${SITE_PACKAGE_DIR}" ]; then
 		PIP="sudo pip3"
 	else
 		PIP="pip3"
 	fi
 	printf "Downloading pip3 formula list... "
-	if ${DOWNLOAD} ${FILE_LOCATION}/pip3-packages ${OUTPUT} ${TMP_DIR}/pip3-packages; then
+	if ${DOWNLOAD} "${FILE_LOCATION}/pip3-packages" ${OUTPUT} "${TMP_DIR}/pip3-packages"; then
 		echo -e "\033[32mDONE\033[0m"
 
 		# Check for missing packages.
-		pip3 freeze > ${TMP_DIR}/pip3-installed
+		pip3 freeze > "${TMP_DIR}/pip3-installed"
 		PACKAGES_TO_INSTALL=()
-		for PACKAGE in $(cat ${TMP_DIR}/pip3-packages); do
-			if ! grep "^${PACKAGE}==" -q ${TMP_DIR}/pip3-installed; then
+		for PACKAGE in $(cat "${TMP_DIR}/pip3-packages"); do
+			if ! grep "^${PACKAGE}==" -q "${TMP_DIR}/pip3-installed"; then
 				echo -e "${PACKAGE} is not installed."
 				PACKAGES_TO_INSTALL+=("${PACKAGE}")
 			fi
@@ -308,4 +308,4 @@ else
 fi
 
 # Tidy up.
-rm -rf ${TMP_DIR}
+rm -rf "${TMP_DIR}"
